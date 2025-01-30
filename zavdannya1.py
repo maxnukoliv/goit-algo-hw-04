a7f9a01f852702648fa56cfebbfb1e2eb1e7252a
MaxNukoliv <91463019+maxnukoliv@users.noreply.github.com>
2025-01-30 13:51:07 +0200

    Update2

--- zavdannya1.py
+++ zavdannya1.py
@@ -4,16 +4,21 @@ path = pathlib.Path(r"C:/Users/W/Documents/vcode/hw-04/goit-algo-hw-04/salary_fi
 
 def total_salary(path):
     try:
-        total = 0 
+        total = 0.0
+        count = 0
+        
         with open(path, mode='r', encoding='utf-8') as file:
             for line in file:
                 parts = line.strip().split(',')
-                salary = int(parts[1].strip())
-                total += int(salary)
-                average = int(total / 2)
+                salary = float(parts[1].strip())
+                total += salary
+                count += 1
+        
+        if count > 0:
+            average = total / count
         print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+        return total, average
     
     except FileNotFoundError:
         print(f"Error: File not found")
-
 total_salary(path)
